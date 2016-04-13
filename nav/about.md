a739e4dbaaed497edda0e409aa1007e752ebc176
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2016-04-13 13:59:17 -0700

    initial commit

---
+++ nav/about.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: About
+permalink: /about/
+nav: 0
+---
+
+Chadwick Designs is committed to using the latest technologies to help businesses connect with clients in new and innovative ways. Although we have experience in almost every aspect of full-stack web development, our specialty is rennovating websites with a responive design so that customers can connect with your site from anywhere. With analytics and SEO, we can optimize interactions with your target audience.
+
+For more information, please <a href="{{ site.baseurl }}/contact" class="link">contact us</a>.
---
+++ nav/contact/contact.php
@@ -0,0 +1,27 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['subject'])     ||
+   empty($_POST['message'])     ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+    echo "No arguments Provided!";
+    return false;
+   }
+    
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+    
+$to = 'info@bartanenconstruction.com';
+$email_subject = "Website Contact:  $subject";
+$email_body = "$name\n$email_address\n\n$subject\n\n$message";
+$headers = "From: $email_address\n";
+$headers .= "Reply-To: $email_address"; 
+if(mail($to,$email_subject,$email_body,$headers)) {
+    return true;
+}
+return false;            
+?>
